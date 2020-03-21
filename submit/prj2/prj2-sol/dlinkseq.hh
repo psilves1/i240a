20a818da8a6756a6837ab6dd31ea0214806237af
Paul <psilvestri9@gmail.com>
2020-03-21 19:14:16 -0400

    Almost done with prj2

--- submit/prj2/prj2-sol/dlinkseq.hh
+++ submit/prj2/prj2-sol/dlinkseq.hh
@@ -1,116 +1,184 @@
-#ifndef DLINK_SEQ_HH_
-#define DLINK_SEQ_HH_
-
-#include "seq.hh"
+#ifndef DLINK_SEQ_H_
+#define DLINK_SEQ_H_
 
 #include <cassert>
 #include <cstddef>
 #include <memory>
 #include <utility>
 
-template <typename E> class ArraySeqConstIter;
+//sequences are wrapped in a unique ptr
+template <typename E> class DLinkSeqConstIter;
 
-
-/** Array Seq implementation */
+/** Seq ADT */
 template <typename E>
 class DLinkSeq : public Seq<E> {
-private:
 
-  int maxSize;    //# of allocated elements
-  int index;      //index one beyond last element
-  E *seq;         //allocated array
-
-  friend class DLinkSeqConstIter<E>;
-
-  enum class Private { TAG, DEFAULT_SIZE = 8 };
 public:
+    
+    DLinkSeq(int size){
+        head = new Node(); //might need to change to raw pointer
+        this->size = size;
+    }
 
-  /** constructor */
-  DLinkSeq(int size, Private tag) :
-    maxSize(size),
-    index(0),
-    seq(new E[size])
-  {}
+    static SeqPtr<E> make(int size = 8) {
+        return std::make_unique<DLinkSeq<E>>(size);
+    }
 
-  static SeqPtr<E> make(int size = static_cast<int>(Private::DEFAULT_SIZE)) {
-    return std::make_unique<ArraySeq<E>>(size, Private::TAG);
+  /** base destructor */
+  ~DLinkSeq() {clear();}
+  
+  /** clear contents from this seq, making it empty. */
+  void clear(){         //Possible memory leak???
+      
+      if(head->next != nullptr){
+          head->next->clear();
+      }
+      if(head->prev != nullptr){
+          head->prev->clear();
+      }
+
+    delete head;
+      
   }
   
-  //destructor  
-  ~DLinkSeq() { delete[] seq; }
-
-  /** empty out the list */
-  void clear() { index = 0; }
-
   /** add item to the start of this seq. */
-  void unshift(const E& item) {
-    for (auto i = index; i > 0; i--) seq[i] = seq[i - 1];
-    ++index;
-    seq[0] = item;
-  }
-  
-  /** add item at the end of this seq. */
-  void push(const E& item) {
-    assert(index < maxSize && "dlinkseq overflow");
-    seq[index++] = item;
-  }
+  void unshift(const E& item){
+      assert(size < maxSize && "dlink seq overflow"); 
+      if(head == nullptr){
+        head = new Node();
+        head->next = nullptr;
+        head->prev = nullptr;
+        head->element = item;
+        size = 1;
+        return;
+      }
+      auto dlink = new Node();
+      dlink->element = item;
+      dlink->next = head;
+      dlink->prev = nullptr;
+      head->prev = dlink;
+      head = dlink;
+      size++;
+      return;
+  };
   
   /** remove and return first item from this seq. 
-   *  fails with failing assert() when this seq is empty.
+   *  fails if this seq is empty.
    */
-  E shift() {
-    assert(index > 0 && "shift on empty dlink seq");
-    E it = seq[0];
-    for (auto i = 0; i < index - 1; i++) {
-      seq[i] = seq[i + 1];
+  E shift(){
+    assert(size > 0 && "shift on empty dlink seq"); 
+    int x = head->element;
+    auto tmp = head;
+    head = head->next;
+    delete tmp; 
+    size--;
+    return element;
+  };
+  
+  /** add item at the end of this seq. */
+  void push(const E& item){
+
+    assert(size < maxSize && "dlink seq overflow");
+
+      if(head == nullptr){
+        head = new Node();
+        head->next = nullptr;
+        head->prev = nullptr;
+        head->element = item;
+        size = 1;
+        return;
+      }
+      
+    auto tmp = new Node({item, nullptr, nullptr});
+
+    auto ptr = head;
+
+    while(ptr->next != nullptr){
+        ptr = ptr->next();
     }
-    --index;
-    return it;
-  }
+
+    ptr->next = tmp;
+    tmp->prev = ptr;
+
+    size++;
+
+  };
   
-  /** remove and return last item from this seq. 
-   *  fails with failing assert() when this seq is empty.
+  /** remove and return last item from this seq.
+   *  fails if this seq is empty.
    */
-  E pop() {
-    assert(index > 0 && "pop on empty dlink seq");
-    return seq[--index];
-  }
+    E pop(){
+        assert(size > 0 && "empty dlink seq");
 
-  /** return number of elements currently in this */
-  int size() const { return index; }
+        auto tmp = new Node({item, nullptr, nullptr});
 
-  /** Return an iterator initialized to start of this seq */
-  ConstIterPtr<E> cbegin() const {
-    const DLinkSeq* constThis = static_cast<const DLinkSeq*>(this);
-    return std::make_unique<DLinkSeqConstIter<E>>(constThis);
-  }
+        auto ptr = head;
 
-  /** Return an iterator initialized to end of this seq */
-  virtual ConstIterPtr<E> cend() const {
-    const DLinkSeq* constThis = static_cast<const DLinkSeq*>(this);
-    return std::make_unique<DLinkSeqConstIter<E>>(constThis, false);
-  }
+        while(ptr->next != nullptr){
+            ptr = ptr->next();
+        }
+
+        int x = ptr->element;
+
+        delete ptr;
+
+        size--;
+
+        return x;
+    }
   
+  /** return number of elements in this seq. */
+  int size(){
+      return size;
+  }
+
+  /** return an iterator initialized to start of this seq */
+  ConstIterPtr<E> cbegin(){
+      
+  }
+
+  /** return an iterator initialized to end of this seq */
+  virtual ConstIterPtr<E> cend() const = 0;
+
+private: 
+
+    typedef struct Node{
+        E element;
+        DLinkSeq* next;
+        DLinkSeq* prev;
+    } Node;
+
+    int maxSize;
+    int size = 0; 
+
+    Node *head;
+
 };
+
+/** const iterator: not possible to change underlying element E */
 
 template <typename E>
 class DLinkSeqConstIter : public ConstIter<E> {
 public:
 
-  DLinkSeqConstIter(const DLinkSeq<E>* const seq, bool isHead=true)
-    : seq(seq),
-      index(isHead ? 0 : seq->index - 1)
-  { }
+  DLinkSeqConstIter(const DLinkSeq<E>* const seq, bool isHead=true){
+      ptr = seq->head;
+      if(!isHead){
+          while(ptr != NULL && ptr->next != NULL){
+              ptr = ptr->next;
+          }
+      }
+  }
 
   /** pre-increment */
   DLinkSeqConstIter& operator++() {
-    ++index;
+    ptr = ptr->next;
     return *this;
   }
 
   /** pre-decrement */
   DLinkSeqConstIter& operator--() {
-    --index;
+    ptr = ptr->prev;
     return *this;
   }
 
@@ -122,22 +190,24 @@ public:
 
   /** return true iff this pointer is not "NULL" */
   virtual operator bool() {
-    return 0 <= index && index < seq->index;
+    return ptr != NULL;
   }
 
   /** Return element this is pointing to */
   const E& operator*() {
-    return seq->seq[index];
+    return ptr->element;
   }
 
   /** Return pointer to element this is pointing to */
   const E* operator->() {
-    return &seq->seq[index];
+    return &ptr->element;
   }
 
 private:
-  const DLinkSeq<E>* const seq;
-  int index;
+  Node *ptr;
 };
 
-#endif //ifndef DLINK_SEQ_HH_
+
+
+
+#endif //ifndef SEQ_H_
