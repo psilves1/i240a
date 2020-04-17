86f71efd75acbeb71efd7273e7255289af006a7c
Paul <psilvestri9@gmail.com>
2020-04-17 16:49:51 -0400

    Fixing prj2

---
+++ submit/lab9/cmake-build-debug/CMakeFiles/lab9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab9.dir/exercises/3-stats/main.cc.o"
+  "CMakeFiles/lab9.dir/exercises/3-stats/ops.cc.o"
+  "CMakeFiles/lab9.dir/exercises/3-stats/sorts.cc.o"
+  "lab9"
+  "lab9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
